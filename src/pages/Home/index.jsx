e8c2354817bfa067362f46d7b2f8102a1f8f9b6f
MatheusRed <contato.matheusdesantana@gmail.com>
2022-12-10 19:53:03 -0300

    added profile Picture and name

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './style.css';
 import { Card } from '../../components/Card';
 
@@ -6,6 +6,7 @@ export function Home() {
 
   const [studentName, setStudentName] = useState("");
   const [students, setStudents] = useState([]);
+  const [user, setUser] = useState({ name: "", avatar: ""});
 
   function handleAddStudent(){
     const newStudent = {
@@ -18,11 +19,31 @@ export function Home() {
     };
     setStudents(prevState => [...prevState, newStudent])
   }
+//O useEffect por padrão executa sempre que a tela for renderizada
+//Ela carrega uma array de dependicias de estados.
+  useEffect(() => {
+    fetch('https://api.github.com/users/matheusred')
+      .then(response => response.json())
+      .then(data => {
+        setUser({
+          name: data.name,
+          avatar: data.avatar_url,
+        })
+      })
+      .catch(e => console.log("Error" + e))
+  }, []);
 
-  
   return (
     <div className='container'>
-      <h1>Lista de Presença</h1>
+      <header>
+        <h1>Lista de Presença</h1>
+        <div>
+          <strong>{user.name}</strong>
+          <img src={user.avatar} alt="Foto de Perfil" />
+        </div>
+      </header>
+      
+      
       <input 
       type="text" 
       placeholder='Digite um nome...'
@@ -34,13 +55,13 @@ export function Home() {
       
       {
         students.map(student => 
-        <Card 
-        key={student.time} 
-        name={student.name} 
-        time={student.time} 
-        />)
-        
-      }
+          <Card 
+          key={student.time} 
+          name={student.name} 
+          time={student.time} 
+          />)
+          
+        }
 
     </div>
   )
